2670cd73a215e33f325834d58e7d7f1f2927a0bf
Allison Sass <allison.sass@gmail.com>
2015-10-04 13:11:50 -0500

    Everything working

--- js/app.js
+++ js/app.js
@@ -1,10 +1,51 @@
 $(document).ready(function() {
 
-		$("#add-button").on('click', addListItem);
+		
+		
+	$("#add-button").on('click', addListItem);
+
 		function addListItem() {
 			var text= $("#add-to-list").val ();
-			alert(text);
+			$("#list").append ('<li class="items"> <div class="box"><div class="empty-box"> </div></div> <p>'
+				+text+'</p> <div class="delete"></div></li>');
+			$("#add-to-list").val('');
 		}
+
+
+	$(document).on ('click', '.delete', deleteItem);
+
+		function deleteItem (){
+			$(this).parent().remove();
+
+		}
+
+
+
+	$('#list').on('mouseenter', 'li', function() {
+			$(this).children('.delete').toggleClass('show');
+
+		});
+
+
+	$('#list').on('mouseleave', 'li', function(){
+			$(this).children('.delete').toggleClass('show');
+		});
+
+
+
+			
+	$('#list').on('click', 'li', function(){
+		$(this).toggleClass('strike'); 
+		$(this).children('.box').toggleClass('checked');
+
+	});
+
+	$('#add-to-list').keydown(function(event){
+		if(event.keyCode == 13) {
+			$('#add-button').click();
+		};
+	});
+
 
 
 
